250b76f7e99bd3d235295047d8f4e2a19872a6c0
DevKayoS <kayoribeirodev@gmail.com>
2025-03-12 00:28:55 -0300

    aprendendo sobre defer

--- main.go
+++ main.go
@@ -1,19 +1,15 @@
 package main
 
-import "fmt"
+import "database/sql"
 
 func main() {
-
 }
 
-func do(x any) {
-	//descobrindo o tipo da variavel
-	switch x.(type) {
-	case string:
-		fmt.Println("que cara das linguas")
-	case int:
-		fmt.Println("que cara de exatas")
-	default:
-		fmt.Println("admin por amor")
+func connectToDb() (*sql.DB, error) {
+	db, err := sql.Open("", "")
+	if err != nil {
 	}
+
+	defer db.Close()
+	return db, nil
 }
---
+++ pacote/defer.go
@@ -0,0 +1,58 @@
+package pacote
+
+import (
+	"fmt"
+	"os"
+)
+
+func algumNomedeFuncao() {
+	x := doDefer()
+	fmt.Println(x)
+}
+
+func doDefer() int {
+	defer fmt.Println("world")
+	fmt.Println("Hello")
+	return 10
+}
+
+func deferFuncao() {
+	x := 10
+	defer func(y int) {
+		fmt.Println(y)
+	}(x)
+
+	x = 50
+	fmt.Println(x)
+}
+
+func doDeferFile() {
+	file, err := os.Open("foo.txt")
+
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	if err := x(); err != nil {
+		panic(err)
+	}
+
+	if err := x(); err != nil {
+		panic(err)
+	}
+}
+
+var arquivos = []string{"foo.txt", "bar.txt", "baz.txt"}
+
+func doDeferEscopo() {
+	func() {
+		for _, f := range arquivos {
+			file, err := os.Open(f)
+			if err != nil {
+			}
+
+			defer file.Close()
+		}
+	}()
+}
